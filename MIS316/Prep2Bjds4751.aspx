06446a5bb07c98eb2ec115a16e1e0d731cc85041
Jacob Sawyer <107064203+jacobsawyer2001@users.noreply.github.com>
2024-04-12 12:02:42 -0400

    Add files via upload

---
+++ MIS316/Prep2Bjds4751.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MIS316/MasterPagejds4751.master" AutoEventWireup="true" CodeFile="Prep2Bjds4751.aspx.cs" Inherits="MIS316_Prep2Bjds4751" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <h1>Multiple of&nbsp; 2 to 1 million?</h1>
+    <p>
+        Enter a Number (max 2,147,483,647):
+        <asp:TextBox ID="txtNumber" runat="server"></asp:TextBox>
+        <asp:RequiredFieldValidator ID="rfvInput" runat="server" ControlToValidate="txtNumber" Display="Dynamic" ErrorMessage="*required" ForeColor="Red" SetFocusOnError="True"></asp:RequiredFieldValidator>
+    </p>
+    <p>
+        <asp:Button ID="btnProcess" runat="server" Text="Process" OnClick="btnProcess_Click" />
+    </p>
+    <p>
+        <asp:Label ID="lblResults" runat="server"></asp:Label>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+</asp:Content>
+
---
+++ MIS316/Prep2Bjds4751.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class MIS316_Prep2Bjds4751 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnProcess_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            int intValueFromUser = 0;
+            int isPrime = 0;
+            intValueFromUser = Convert.ToInt32(txtNumber.Text);
+            lblResults.Text = "";
+            for (int i = 2; i <= 1000000; i++)
+            {
+                if (IsMultipleOf(intValueFromUser, i) == true)
+                {
+                    lblResults.Text += txtNumber.Text + " is a multiple of " + i.ToString() + "<br>";
+                    isPrime++;
+                }
+            }
+
+            if (isPrime == 0)
+            {
+                lblResults.Text = PrimeNumber(0) + ".";
+            }
+            if (isPrime == 1)
+            {
+                lblResults.Text = PrimeNumber(1);
+            }
+        }
+        catch(Exception)
+        {
+            lblResults.Text = "Could not factor this number. Remove non-numerical characters or try a lower number.";
+        }
+    }
+
+    protected bool IsMultipleOf(int intNumberToTest, int intRoot)
+    {
+        bool blnIsMultipleOf = false;
+        if (intNumberToTest % intRoot == 0)
+        {
+            blnIsMultipleOf = true;
+        }
+
+        return blnIsMultipleOf;
+    }
+
+    protected string PrimeNumber(int i)
+    {
+        string strOutput = txtNumber.Text + " is not a multiple of any numbers from 2 to 1 million";
+        if (i == 1)
+        {
+            strOutput += " except itself.";
+        }
+        return strOutput;
+    }
+}
